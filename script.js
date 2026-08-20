8074518e7c52ad0efff49aa4ea8271fe97c5bbac
vitorcosta <vitorsfc26@gmail.com>
2026-08-20 13:58:34 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+const expenseList = document.querySelector("ul")
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
+}
+
+
+
+
+form.onsubmit = () => {
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "Remover")
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
+
