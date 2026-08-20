b23d7a0262c26ad7bb943053e830975b59c284c3
vitorcosta <vitorsfc26@gmail.com>
2026-08-20 16:38:38 -0300

    final commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
 const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
@@ -21,9 +23,6 @@ function formatCurrencyBRL(value){
 
     return value
 }
-
-
-
 
 form.onsubmit = () => {
     event.preventDefault()
@@ -72,9 +71,65 @@ function expenseAdd(newExpense) {
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
 
+        formClear()
+
+        updateTotals()
+
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
         console.log(error)
     }
 }
 
+function updateTotals() {
+    try {
+        const items = expenseList.children
+        
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        let total = 0
+
+        for(let item = 0; item < items.length; item++){
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
+
+            value = parseFloat(value)
+
+            if(isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número")
+            }
+
+            total += Number(value)
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+        expensesTotal.innerHTML = ""
+
+        expensesTotal.append(symbolBRL, total)
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais")
+    }
+}
+
+expenseList.addEventListener("click", function (event) {
+    if(event.target.classList.contains("remove-icon")) {
+        const item = event.target.closest(".expense")
+        item.remove()
+    }
+
+    updateTotals()
+})
+
+function formClear() {
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+
+    expense.focus()
+}
